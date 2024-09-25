a7678d23b392f709386ac66a59484dac02103147
Lindsay Ward <lindsay.ward@jcu.edu.au>
2024-09-26 09:49:52 +1000

    Add seminar 1 GST program

---
+++ seminar01.py
@@ -0,0 +1,16 @@
+# number = int(input("Number: "))
+# if number < 0:
+#     print(f"{number} is negative")
+# else:
+#     print(f"{number} is not negative")
+# print("Done, yeah!!!!")
+GST_RATE = 0.1
+
+tax_amount = 0
+item_price = float(input('Enter item price: $'))
+gst_status = input('Does it have GST? (Y/N) ').upper()
+if gst_status == 'Y':
+    tax_amount = item_price * GST_RATE
+final_price = item_price + tax_amount
+
+print(f"Your item costs ${final_price:.2f}")
